4371f77f1ff2da9e9cfe39fd7098b6f07a2b18b5
salmaloum <goforsalma@gmail.com>
2023-02-13 18:32:51 -0800

    functioning header and footer

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-import Auth from '../../utils/auth';
+import Auth from '../../utils/auth'
 
 const Header = () => {
   const logout = (event) => {
-    event.preventDefault();
-    Auth.logout();
-  };
+    event.preventDefault()
+    Auth.logout()
+  }
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Thoughts</h1>
+            <h1 className="m-0">Etiquette</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
+          <p className="m-0">Book your nail salon appointments today!</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
@@ -40,7 +40,7 @@ const Header = () => {
         </div>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
