b06871e64be91d1559e25a5c3bb94c3d4214574c
bharatrai53 <bharatrai2016@gmail.com>
2023-02-14 10:31:37 -0800

    got homepage to render

---
+++ client/src/components/SalonForm/index.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+
+import { ADD_SALON } from '../../utils/mutations';
+import { QUERY_SALONS } from '../../utils/queries';
+
+import Auth from '../../utils/auth';
+
+const SalonForm = () => {
+    const SalonForm = () => {
+        const [salonName, setSalonName] = useState('')
+        const [salonAddress, setSalonAddress] = useState('')
+        const [artist, setArtist] = useState('')
+        const [availability, setAvailability] = useState('')
+        const [salonHours, setSalonHours] = useState('')
+        const [error, setError] = useState('')
+
+        const [addSalon, { error: addSalonError }] = useMutation(ADD_SALON, {
+            update(cache, { data: { addSalon } }) {
+                try {
+                    const { salons } = cache.readQuery({ query: QUERY_SALONS })
+
+                    cache.writeQuery({
+                        query: QUERY_SALONS,
+                        data: { salons: [addSalon, ...salons] },
+                    })
+                } catch (e) {
+                    console.error(e)
+                }
+
+                const { me } = cache.readQuery({ query: QUERY_ME })
+                cache.writeQuery({
+                    query: QUERY_ME,
+                    data: { me: { ...me, salons: [...me.salons, addSalon] } },
+                })
+            },
+        })
+
+        const handleFormSubmit = async (event) => {
+            event.preventDefault()
+
+            try {
+                const { data } = await addSalon({
+                    variables: {
+                        salonName,
+                        salonAddress,
+                        artist,
+                        availability,
+                        salonHours,
+                    },
+                })
+
+                setSalonName('')
+                setSalonAddress('')
+                setArtist('')
+                setAvailability('')
+                setSalonHours('')
+                setError('')
+            } catch (err) {
+                setError(err.message)
+            }
+        }
+    }
+}
--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,15 +1,21 @@
 const db = require('../config/connection');
-const { User } = require('../models');
+const { User, Salon, Appointment, Service } = require('../models');
 const userSeeds = require('./userSeeds.json');
+// const serviceSeeds = require('./serviceSeeds.json')
+const salonSeeds = require('./salonSeeds.json')
+const appointmentSeeds = require('./appointmentSeeds.json')
 // const thoughtSeeds = require('./thoughtSeeds.json');
-
 db.once('open', async () => {
   try {
     // await Thought.deleteMany({});
     await User.deleteMany({});
-
+    await Salon.deleteMany({});
+    await Appointment.deleteMany({})
+    // await Service.deleteMany({})
     await User.create(userSeeds);
-
+    await Salon.create(salonSeeds);
+    await Appointment.create(appointmentSeeds)
+    // await Service.create(serviceSeeds)
     // for (let i = 0; i < thoughtSeeds.length; i++) {
     //   const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
     //   const user = await User.findOneAndUpdate(
@@ -25,7 +31,6 @@ db.once('open', async () => {
     console.error(err);
     process.exit(1);
   }
-
   console.log('all done!');
   process.exit(0);
 });
