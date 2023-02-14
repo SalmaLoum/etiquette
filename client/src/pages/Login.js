7f9f675ce8ff248aff823d7ad81fbb538299b19c
salmaloum <goforsalma@gmail.com>
2023-02-14 12:49:44 -0800

    updated css login/signup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,44 +1,44 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../utils/mutations';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useMutation } from '@apollo/client'
+import { LOGIN_USER } from '../utils/mutations'
 
-import Auth from '../utils/auth';
+import Auth from '../utils/auth'
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error, data }] = useMutation(LOGIN_USER);
+  const [formState, setFormState] = useState({ email: '', password: '' })
+  const [login, { error, data }] = useMutation(LOGIN_USER)
 
   // update state based on form input changes
   const handleChange = (event) => {
-    const { name, value } = event.target;
+    const { name, value } = event.target
 
     setFormState({
       ...formState,
       [name]: value,
-    });
-  };
+    })
+  }
 
   // submit form
   const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
+    event.preventDefault()
+    console.log(formState)
     try {
       const { data } = await login({
         variables: { ...formState },
-      });
+      })
 
-      Auth.login(data.login.token);
+      Auth.login(data.login.token)
     } catch (e) {
-      console.error(e);
+      console.error(e)
     }
 
     // clear form values
     setFormState({
       email: '',
       password: '',
-    });
-  };
+    })
+  }
 
   return (
     <main className="flex-row justify-center mb-4">
@@ -70,7 +70,7 @@ const Login = (props) => {
                   onChange={handleChange}
                 />
                 <button
-                  className="btn btn-block btn-primary"
+                  className="btn btn-block btn-dark"
                   style={{ cursor: 'pointer' }}
                   type="submit"
                 >
@@ -88,7 +88,7 @@ const Login = (props) => {
         </div>
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client'
+import { ADD_USER } from '../utils/mutations'
 
-import Auth from '../utils/auth';
+import Auth from '../utils/auth'
 
 const Signup = () => {
   const [formState, setFormState] = useState({
     username: '',
     email: '',
     password: '',
-  });
-  const [addUser, { error, data }] = useMutation(ADD_USER);
+  })
+  const [addUser, { error, data }] = useMutation(ADD_USER)
 
   const handleChange = (event) => {
-    const { name, value } = event.target;
+    const { name, value } = event.target
 
     setFormState({
       ...formState,
       [name]: value,
-    });
-  };
+    })
+  }
 
   const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
+    event.preventDefault()
+    console.log(formState)
 
     try {
       const { data } = await addUser({
         variables: { ...formState },
-      });
+      })
 
-      Auth.login(data.addUser.token);
+      Auth.login(data.addUser.token)
     } catch (e) {
-      console.error(e);
+      console.error(e)
     }
-  };
+  }
 
   return (
     <main className="flex-row justify-center mb-4">
@@ -76,7 +76,7 @@ const Signup = () => {
                   onChange={handleChange}
                 />
                 <button
-                  className="btn btn-block btn-primary"
+                  className="btn btn-block btn-dark"
                   style={{ cursor: 'pointer' }}
                   type="submit"
                 >
@@ -94,7 +94,7 @@ const Signup = () => {
         </div>
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default Signup;
+export default Signup
