3b0341c577a824de58f264e6c77104c43346fa22
Kayla Casale <kayla.casale@gmail.com>
2023-02-14 12:32:31 -0800

    pushing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -24,16 +24,16 @@ const Home = () => {
         </div>
 
         <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
+          <div
+            className="col-12 col-md-10 mb-3 p-3"
+            style={{ border: '1px dotted #1a1a1a' }}
           ></div>
           <SalonForm />
         </div>
-      
+
       </div>
     </main>
   )
 }
 
-export default Home
+export default Home;
