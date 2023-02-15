c278510b039bd5bbf167f41ef05d34d572ccc9cd
salmaloum <goforsalma@gmail.com>
2023-02-15 12:00:38 -0800

    updated css for salon page with cloudinary

--- client/src/components/SalonForm/index.js
+++ client/src/components/SalonForm/index.js
@@ -46,10 +46,31 @@ const SalonForm = ({ salonId }) => {
       setError(err.message)
     }
   }
+  // cloudinary
+  var openWidget = (e) => {
+    e.preventDefault()
+    myWidget.open()
+  }
+  var myWidget = window.cloudinary.createUploadWidget(
+    {
+      cloudName: 'do6kan0iu',
+      uploadPreset: 'etiquette',
+    },
+    (error, result) => {
+      if (!error && result && result.event === 'success') {
+        console.log('Done! Here is the image info: ', result.info)
+      }
+    },
+  )
 
   return (
-    <div>
-      <h3> Please add your Salon!</h3>
+    <div className="card">
+      <h4 className="card-header text-center bg-dark text-light p-1">
+        Please add your Salon!
+      </h4>
+      <p className=" text-center text-black p-2">
+        If it's not on the list above
+      </p>
 
       {Auth.loggedIn() ? (
         <>
@@ -57,47 +78,55 @@ const SalonForm = ({ salonId }) => {
             className="flex-row justify-center justify-space-between-md align-center"
             onSubmit={handleFormSubmit}
           >
-            <div className="col-12 col-lg-12">
-              <textarea
-                name="salonName"
-                placeholder="Add Salon Name"
-                value={salonName}
-                className="form-input w-100"
-                style={{ lineHeight: '1.5', resize: 'vertical' }}
-                onChange={(event) => setSalonName(event.target.value)}
-              ></textarea>
+            <textarea
+              name="salonName"
+              placeholder="Add Salon Name"
+              value={salonName}
+              className="form-input w-100"
+              style={{ lineHeight: '1.5', resize: 'vertical' }}
+              onChange={(event) => setSalonName(event.target.value)}
+            ></textarea>
 
-              <textarea
-                name="salonAddress"
-                placeholder="Add Salon Address"
-                value={salonAddress}
-                className="form-input w-100"
-                style={{ lineHeight: '1.5', resize: 'vertical' }}
-                onChange={(event) => setSalonAddress(event.target.value)}
-              ></textarea>
+            <textarea
+              name="salonAddress"
+              placeholder="Add Salon Address"
+              value={salonAddress}
+              className="form-input w-100"
+              style={{ lineHeight: '1.5', resize: 'vertical' }}
+              onChange={(event) => setSalonAddress(event.target.value)}
+            ></textarea>
 
-              <textarea
-                name="salonHours"
-                placeholder="Add Salon Hours"
-                value={salonHours}
-                className="form-input w-100"
-                style={{ lineHeight: '1.5', resize: 'vertical' }}
-                onChange={(event) => setSalonHours(event.target.value)}
-              ></textarea>
-
-              <button className="btn btn-dark btn-lg py-3" type="submit">
+            <textarea
+              name="salonHours"
+              placeholder="Add Salon Hours"
+              value={salonHours}
+              className="form-input w-100"
+              style={{ lineHeight: '1.5', resize: 'vertical' }}
+              onChange={(event) => setSalonHours(event.target.value)}
+            ></textarea>
+            <div>
+              <button
+                id="upload_widget"
+                class="cloudinary-button btn btn-light btn-lg py-3"
+                onClick={openWidget}
+              >
+                Upload salon images
+              </button>
+            </div>
+            <div class="btn btn-block btn-dark m-4">
+              <button class="btn btn-block btn-dark" type="submit">
                 Add Salon
               </button>
-              {error && (
-                <div className="col-12 my-3 bg-danger text-white p-3">
-                  {error.message}
-                </div>
-              )}
             </div>
+            {error && (
+              <div className="col-12 my-3 bg-danger text-white p-3">
+                {error.message}
+              </div>
+            )}
           </form>
         </>
       ) : (
-        <p>
+        <p class="bold">
           You need to be logged in as an Admin to add a Salon. Please{' '}
           <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
         </p>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,6 @@ import SalonList from '../components/SalonList'
 
 import SalonForm from '../components/SalonForm'
 
-
 import { QUERY_SALONS } from '../utils/queries'
 
 const Home = () => {
@@ -15,34 +14,21 @@ const Home = () => {
 
   return (
     <main>
+      <div className="flex-row justify-center border mb-3">
+        <div className="col-12 col-md-8 mb-3 align-center ">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <SalonList salons={salons} title="Available Salons" />
+          )}
+        </div>
+      </div>
       <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <div className="col-12 col-md-8 mb-3">
-            {loading ? (
-              <div>Loading...</div>
-            ) : (
-
-              <SalonList
-                salons={salons}
-                title="Available Salons" />
-            )}
-          </div>
-        </div>
-
-        <div className="flex-row justify-center">
-          <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
-          ></div>
-          <SalonForm />
-        </div>
-
+        <div className="col-12 col-md-10 mb-3 p-3"></div>
+        <SalonForm />
       </div>
     </main>
   )
 }
 
-export default Home;
+export default Home
