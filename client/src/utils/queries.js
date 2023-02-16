56bbd04d946b5d611ec772041c1550bdfb9f7053
Kayla Casale <kayla.casale@gmail.com>
2023-02-16 15:12:55 -0800

    added user query to queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,18 +1,14 @@
 import { gql } from '@apollo/client'
 
 export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
-      _id
-      username
-      email
-      thoughts {
-        _id
-        thoughtText
-        createdAt
-      }
-    }
+query Query($username: String!) {
+  user(username: $username) {
+    _id
+    username
+    email
+    password
   }
+}
 `
 
 export const QUERY_SALONS = gql`
